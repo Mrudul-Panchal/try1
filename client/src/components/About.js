4bbf7f2ea774b78d05b03bdab43b5c22d5e6a9d8
Mrudul-Panchal <mrudul@webcluesinfotech.com>
2021-08-10 13:11:56 +0530

    commit on 'master'

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,27 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
 
 const About = () => {
 
     const history = useHistory();
+    const [userData, serUserData] = useState();
 
     const callAboutPage = async () => {
-        try{
+        try {
             const res = fetch('/about', {
                 method: 'GET',
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json"
                 },
-                credentials:"include"
+                credentials: "include"
             });
 
             const data = await res.json();
             console.log(data);
+            serUserData(data);
 
-            if( !res.status === 200){
+            if (!res.status === 200) {
                 const error = new Error(res.error);
                 throw error;
+            } else {
+                return About;
             }
 
         } catch (err) {
@@ -30,9 +34,9 @@ const About = () => {
         }
     }
 
-useEffect(() => {
-    callAboutPage();
-});
+    useEffect(() => {
+        callAboutPage();
+    });
 
     return (
         <div>
